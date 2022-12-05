79d3b013075f26f02d250b3c09f32473ea67635f
TakayanagiHAL <takayanagi.hal.at@gmail.com>
2022-12-05 17:47:13 +0900

    UIマネージャー作成

---
+++ Assets/UI/UIManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public enum CanvasName
+{
+    SCORE_UI,
+    RESOURCE_UI,
+    COMMAND_UI
+}
+
+[Serializable]
+public struct CanvasPair
+{
+    public CanvasName canvasName;
+    public Canvas canvas;
+}
+
+[Serializable]
+public class CanvasDictionary
+{
+    public List<CanvasPair> keyValuePairs;
+
+    public Canvas GetCanvas(CanvasName name)
+    {
+        for (int i = 0; i < keyValuePairs.Count; i++)
+        {
+            if (name == keyValuePairs[i].canvasName)
+            {
+                return keyValuePairs[i].canvas;
+            }
+        }
+        return null;
+    }
+}
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] CanvasDictionary canvasDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCanvas(CanvasName name,bool flag)
+    {
+        Canvas canvas = canvasDictionary.GetCanvas(name);
+        canvas.enabled = flag;
+    }
+
+    public Canvas GetCanvas(CanvasName name)
+    {
+        return canvasDictionary.GetCanvas(name);
+    }
+}
