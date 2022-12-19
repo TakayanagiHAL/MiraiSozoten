8fdbfcd76fa477ea9b72704b66fe6eae76937775
TakayanagiHAL <takayanagi.hal.at@gmail.com>
2022-12-19 17:44:00 +0900

    UI設定

--- Assets/UI/ScoreUI.cs
+++ Assets/UI/ScoreUI.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour
 {
+    [SerializeField] Text medalText;
+    [SerializeField] Text moneyText;
+    [SerializeField] Text oilText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,23 @@ public class ScoreUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetMoney(int money)
+    {
+        moneyText.text = money.ToString();
+    }
+
+    public void SetOil(SeaResource seaResource)
+    {
+        int oil = 0;
+
+        oil += seaResource.plastic;
+        oil += seaResource.ePlastic*3;
+        oil += seaResource.wood*4;
+        oil += seaResource.steel*15;
+        oil += seaResource.seaFood*12;
+
+        oilText.text = oil.ToString();
     }
 }
