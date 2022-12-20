decb5d47dea99ec76fefe60e2cc42924b2ec582c
Shoto <shoto1102hal91303@outlook.jp>
2022-12-21 00:05:44 +0900

    雲の更新

--- Assets/honma/Script/TitleCloudBone.cs
+++ Assets/honma/Script/TitleCloudBone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TitleCloudBone : MonoBehaviour
 {
@@ -15,18 +16,21 @@ public class TitleCloudBone : MonoBehaviour
 
     [Header("----------------------")]
     [SerializeField]
-    [Range(-1500.0f, 0.0f)] private float MinPosition_X;
+    [Range(-1200.0f, -600.0f)] private float MinPosition_X;
     [SerializeField]
-    [Range(0.0f, 600.0f)] private float MaxPosition_X;
+    [Range(-1100.0f, -500.0f)] private float MaxPosition_X;
+    
     [SerializeField]
-    [Range(-600.0f, 0.0f)] private float MinPosition_W;
+    [Range(-1500.0f, 0.0f)] private float MinPosition_W;
     [SerializeField]
-    [Range(0.0f, 600.0f)] private float MaxPosition_W;
+    [Range(0.0f, 1500.0f)] private float MaxPosition_W;
 
     [SerializeField]     private float MinPosition_Y;
     [SerializeField]     private float MaxPosition_Y;
 
-    
+    //ki-
+    Keyboard keyboard;
+    int putA = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +38,16 @@ public class TitleCloudBone : MonoBehaviour
         _cloudArray = new GameObject[_cloudAmount];
 
         Spawn();
+
+        // 現在のキーボード情報
+        keyboard = Keyboard.current;
+        // キーボード接続チェック
+        if (keyboard == null)
+        {
+            // キーボードが接続されていないと
+            // Keyboard.currentがnullになる
+            return;
+        }
     }
 
     private void Spawn()
@@ -56,39 +70,62 @@ public class TitleCloudBone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 1キーの入力状態取得
+        var Key_a = keyboard.aKey;
         
-    }
-
-    private void FixedUpdate()
-    {
-        
-
-        if(_cloudArray.Length<_cloudAmount)//雲の数が総量を下回った時
+        if (Key_a.wasPressedThisFrame)
         {
-            float x = Random.Range(MinPosition_X, MaxPosition_X);
-            float w = Random.Range(MinPosition_W, MaxPosition_W);
-            float y = Random.Range(MinPosition_Y, MaxPosition_Y);
+            Destroy(_cloudArray[putA]);
+            putA += 1;
+            if(putA>=2)
+            {
+                float x = Random.Range(MinPosition_X, MaxPosition_X);
+                float w = Random.Range(MinPosition_W, MaxPosition_W);
 
-            Vector3 position = new Vector3(x, y, w);
-            _cloudArray[CloudArraySerch()] = Instantiate(_cloudObject, position, Quaternion.identity);
+                float y = Random.Range(MinPosition_Y, MaxPosition_Y);
+
+                Vector3 position = new Vector3(x, y, w);
+                _cloudArray[putA-2] = Instantiate(_cloudObject, position, Quaternion.identity, this.gameObject.transform);
+            }
+           
         }
     }
 
+    //private void FixedUpdate()
+    //{
+    //    // 1キーの入力状態取得
+    //    var Key_b = keyboard.bKey;
+    //    Debug.Log("雲の総量がAmount以下のとき"+_cloudArray.Length);
 
-    //空の要素を探し、instantiateする為の空の要素の番号を返す
-    private int CloudArraySerch()
-    {
-        GameObject[] gameArray = _cloudArray;
-        int index = 0;
+    //    if(CloudArraySerch()>0)//雲の数が総量を下回った時
+    //    {
+    //        //int a = CloudArraySerch();
+            
+    //        float x = Random.Range(MinPosition_X, MaxPosition_X);
+    //        float w = Random.Range(MinPosition_W, MaxPosition_W);
+    //        float y = Random.Range(MinPosition_Y, MaxPosition_Y);
+
+    //        Vector3 position = new Vector3(x, y, w);
+    //        //Debug.Log("雲のi:"+ CloudArraySerch());
+    //        _cloudArray[CloudArraySerch()] = Instantiate(_cloudObject, position, Quaternion.identity, this.gameObject.transform);
+    //    }
+    //}
 
 
-        for(int i=0;i<_cloudAmount;i++)
-        {
-            if (gameArray[i] == null)
-                index = i;
-                break;
-        }
+    ////空の要素を探し、instantiateする為の空の要素の番号を返す
+    //private int CloudArraySerch()
+    //{
+    //    GameObject[] gameArray = _cloudArray;
+    //    int index = _cloudAmount + 1;
 
-        return index;
-    }
+
+    //    for (int i = 0; i <= _cloudAmount; i++)
+    //    {
+    //        if (gameArray[i] == null)
+    //            return i;
+    //    }
+
+
+    //    return index;
+    //}
 }
--- Assets/honma/Script/VFXCloud.cs
+++ Assets/honma/Script/VFXCloud.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXCloud : MonoBehaviour
 {
+    [SerializeField]
+    private float CloudVelocityX;
+    [SerializeField]
+    private float CloudVelocityY;
+    [SerializeField][Header("â_ÇÃçÇÇ≥êßå¿")]
+    private float CloudLimitPositionY;
+
     private Renderer _Renderer;
+    private VisualEffect _VFXCloud;
 
     // Start is called before the first frame update
     void Start()
     {
         _Renderer = GetComponent<Renderer>();
+        _VFXCloud = GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
@@ -19,9 +29,22 @@ public class VFXCloud : MonoBehaviour
         {
            // Destroy(this.gameObject);
         }
+
+        Transform myTransform = this.transform;
+        Vector3 myVector3 = myTransform.position;
+
+        myVector3.x += CloudVelocityX;
+        if (CloudLimitPositionY >= myTransform.position.y)
+        {
+            myVector3.y += CloudVelocityY;
+        }
+
+        myTransform.position = myVector3;
+
     }
     private void OnBecameInvisible()
     {
         Destroy(this.gameObject);
+        
     }
 }
