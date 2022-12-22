b94749118c6c271b80ee3c5819dc0ca68b27676f
Shoto <shoto1102hal91303@outlook.jp>
2022-12-22 21:32:43 +0900

    タイトル用のスクリプト追加

--- Assets/honma/Script/TitleObjectMoving.cs
+++ Assets/honma/Script/TitleObjectMoving.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class TitleObjectMoving : MonoBehaviour
 {
     [SerializeField]
-    [Header("prefabから動かしたいオブジェクトを追加")]
+    [Header("prefabから動かしたいオブジェクトを追加(岩など)")]
     private List<GameObject> _movingObjectList;
+
+    [SerializeField]
+    [Header("この半径でオブジェクトを避ける")]
+    private float _playerRadius;
+
+    [SerializeField]
+    [Header("実際には岩のスピード")]
+    private float _playerMoveSpeed;
+
 
     // Start is called before the first frame update
     void Start()
