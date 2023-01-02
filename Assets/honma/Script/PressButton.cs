0224dde01030dfb2901d509c76ebc0b6747a3a17
Shoto <shoto1102hal91303@outlook.jp>
2023-01-02 20:31:13 +0900

    シーン遷移間のLodingオブジェクト作成

---
+++ Assets/honma/Script/PressButton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PressButton : MonoBehaviour
+{
+    [SerializeField][Header("ClickLoadSceneのscriptを持ってくる")]
+    private NextSceneLoad _nextSceneLoadScript;
+
+    Keyboard _keyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 現在のキーボード情報
+        _keyboard = Keyboard.current;
+
+        // キーボード接続チェック
+        if (_keyboard == null)
+        {
+            // キーボードが接続されていないと
+            // Keyboard.currentがnullになる
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bキーの入力状態取得
+        var Key_putB = _keyboard.bKey;
+
+        if (Key_putB.wasPressedThisFrame)
+        {
+            _nextSceneLoadScript.LoadSceneStart();
+        }
+    }
+}
