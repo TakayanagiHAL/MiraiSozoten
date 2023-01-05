8593bf8494474278f8b21b5d4693cf9d5a8e9b04
Fujiwara3497 <fujiwara.seiya.1227@gmail.com>
2023-01-06 00:10:22 +0900

    ロビーシーンの機能追加 ・退室機能 ・プレイヤー入室状態

--- Assets/fujiwara/Scripts/LobbyScene/UI_RuleSetting.cs
+++ Assets/fujiwara/Scripts/LobbyScene/UI_RuleSetting.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class UI_RuleSetting : StrixBehaviour
 {
+
+    [SerializeField] private GameObject[] PlayerIcons = new GameObject[4];
+
     [SerializeField] private GameObject RoomID;
 
     // Start is called before the first frame update
@@ -17,11 +20,42 @@ public class UI_RuleSetting : StrixBehaviour
 
         RoomID.GetComponent<Text>().text = idText;
 
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //プレイヤーリストの表示更新
+        int memberCount = StrixNetwork.instance.room.GetMemberCount();
+
+        int count = 0;
+        foreach(var roomMember in StrixNetwork.instance.sortedRoomMembers)
+        {
+            Text name = PlayerIcons[count].transform.Find("PlayerNameBoard").gameObject.transform.Find("PlayerNameText").GetComponent<Text>();
+
+            name.text = roomMember.GetName();
+            PlayerIcons[count].transform.Find("PlayerLobbyStatus").gameObject.SetActive(true);
+            count++;
+        }
+
+        //不足分（NPC）
+        if(memberCount<4)
+        {
+            while(count<4)
+            {
+                Text name = PlayerIcons[count].transform.Find("PlayerNameBoard").gameObject.transform.Find("PlayerNameText").GetComponent<Text>();
+                name.text = "NPC";
+                PlayerIcons[count].transform.Find("PlayerLobbyStatus").gameObject.SetActive(false);
+                count++;
+               
+            }
+            
+        }
     }
+
+    
+
 }
