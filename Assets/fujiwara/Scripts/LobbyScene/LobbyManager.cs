ad47a2ee7708bf3c6efb22281ec8ac98ceb3c1b5
Fujiwara3497 <fujiwara.seiya.1227@gmail.com>
2023-01-05 17:42:30 +0900

    no message

--- Assets/fujiwara/Scripts/LobbyScene/LobbyManager.cs
+++ Assets/fujiwara/Scripts/LobbyScene/LobbyManager.cs
@@ -6,21 +6,27 @@ using SoftGear.Strix.Unity.Runtime;
 
 public class LobbyManager : StrixBehaviour
 {
-
+    [SerializeField] private GameObject UI_RuleSettingCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject gameStartButton = GameObject.Find("PrivateUI_Canvas").gameObject.transform.Find("MatchConfirmButton").gameObject;
+
         //ホストにのみゲームスタートボタンを表示させる
         if(StrixNetwork.instance.isRoomOwner)
         {
             gameStartButton.SetActive(true);
+
+            Instantiate(UI_RuleSettingCanvas);
+
         }
         else
         {
             gameStartButton.SetActive(false);
         }
+
+
     }
 
     // Update is called once per frame
--- Assets/fujiwara/Scripts/LobbyScene/UI_RuleSetting.cs
+++ Assets/fujiwara/Scripts/LobbyScene/UI_RuleSetting.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SoftGear.Strix.Unity.Runtime;
+using UnityEngine.UI;
 
-public class UI_RuleSetting : MonoBehaviour
+public class UI_RuleSetting : StrixBehaviour
 {
+    [SerializeField] private GameObject RoomID;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ÉãÅ[ÉÄIDÇÃï\é¶
+        long id = StrixNetwork.instance.selfRoomMember.GetRoomId();
+        string idText = id.ToString();
+
+        RoomID.GetComponent<Text>().text = idText;
+
     }
 
     // Update is called once per frame
